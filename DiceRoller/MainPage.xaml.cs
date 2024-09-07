283859ac74473e4b4776793d687eefc278c48d16
Nayda <naluna909@gmail.com>
2024-09-06 22:47:20 -0300

    Atualizaçao

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -7,6 +7,23 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
+    private void DiceRoller_Clicked(object sender, EventArgs e)
+    {
+        // int numeroaleatorio
+        //()1+1
+        int numeroSelecionado = Convert.ToInt32(SidesPicker.SelectedItem.ToString());
+        int numeroSorteado = new Random().Next(1, numeroSelecionado + 1);
+        ResultLabel.Text = numeroSorteado.ToString();
+    }
+
+
+
+  //  private void DiceSide_Clicked(object sender, EventArgs e)
+  //  {
+
+
+  //  }
+
     //Sortear um número aleatório através do clique do botão
     //Devolver o número sorteado para a interface
     //Conseguir selecionar a quantidade de lados a partir do Picker
